8b17d89afd12cd189093a71c1c318ece14d7fa71
fernandopaz1 <ferpaz9292@gmail.com>
2021-11-07 17:55:45 -0300

    feat: sidemenu sacado de tutorial

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,12 @@
+const btnBurger = document.getElementById("menu-btn");
+const asideMenu = document.getElementById("sidemenu");
+
 btnMenu.addEventListener("click", (e) => {
-    window.open('./src/CV_MELINAZELAYA.pdf', '_blank', 'fullscreen=yes'); return false;
+    window.open("./src/CV_MELINAZELAYA.pdf", "_blank", "fullscreen=yes");
+    return false;
+});
+
+btnBurger.addEventListener("click", (e) => {
+    asideMenu.classList.toggle("menu-expanded");
+    asideMenu.classList.toggle("menu-collapsed");
 });
